33bdd1ff6a99d729b217f3ffa46a754840791b82
amerjali2022 <jose.ahumada.mt@usa.edu.co>
2022-09-22 19:40:28 -0500

    Version Inicial

---
+++ usa/src/main/java/co/edu/usa/ProyectoCostumesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCostumesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCostumesApplication.class, args);
+	}
+
+}
